78e85083daadac451ec201e6d1d9c4cebd45a3c5
hende8 <46380703+hende8@users.noreply.github.com>
2020-05-02 14:07:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ $( document ).ready(function() {
       users=new Array();
       var user1=new User("p","p","babi","H@.com","");
       users.push(user1);
+    console.log("ready" + users);
       if(localStorage.getItem("users"==null)){
         localStorage.setItem("users",JSON.stringify(users));
       }
@@ -24,9 +25,12 @@ function addUser(user){
     users.push(user);
     localStorage.removeItem("users");
     localStorage.setItem("users",JSON.stringify(users));
+        console.log("addUser" + users);
+
 }
 function getUser(userName){ 
        users=  JSON.parse(localStorage.getItem("users"));
+       console.log("getUser" + users);
         for(var i=0;i<users.length;i++){
             if(users[i].username==userName){
                 return users[i];
